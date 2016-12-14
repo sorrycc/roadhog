0ea523ebec1dc442d4a60b9d25fbb8a7b5366ceb
sorrycc <sorrycc@gmail.com>
2016-12-14 15:03:12 +0800

    comment is allowed in .roadhogrc

--- utils/getConfig.js
+++ utils/getConfig.js
@@ -1,13 +1,14 @@
 const pathExists = require('path-exists');
 const fs = require('fs');
 const explain = require('explain-error');
+const stripJsonComments = require('strip-json-comments');
 const paths = require('../config/paths');
 
 module.exports = function(fileName = '.roadhogrc') {
   const configPath = paths.resolveApp(fileName);
   if (pathExists.sync(configPath)) {
     try {
-      return JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+      return JSON.parse(stripJsonComments(fs.readFileSync(configPath, 'utf-8')));
     } catch (e) {
       throw explain(e, `Config path ${fileName} parse error.`);
     }
