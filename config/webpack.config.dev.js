5ed9a4c0f2ee0a396cb713879759cd979c616eb2
sorrycc <sorrycc@gmail.com>
2016-12-15 11:10:52 +0800

    😇

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -102,7 +102,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': process.env.NODE_ENV,
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       },
     }),
     new webpack.HotModuleReplacementPlugin(),
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -105,7 +105,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': process.env.NODE_ENV,
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       },
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
