1905bfc56c1100475202a8d288f3de51f0cf12d2
哈喇子 <252133226@qq.com>
2017-02-24 10:14:55 +0800

    添加 `_mocha` 本地/全局目录判断 (#147)
    * 添加 `_mocha` 本地/全局 判断
    
    * `_mocha` 目录修改

--- src/test.js
+++ src/test.js
@@ -17,7 +17,7 @@ const argv = require('yargs')
 const compiler = join(__dirname, './test/compiler.js');
 const setup = join(__dirname, './test/setup.js');
 const mochaArgs = process.argv.slice(2).filter(item => item !== '--coverage').join(' ');
-const mochaBin = join(__dirname, '../node_modules/.bin/_mocha');
+const mochaBin = require.resolve('mocha/bin/_mocha');
 const istanbul = join(require.resolve('istanbul'), '../lib/cli.js');
 const cmd = argv.coverage ?
   `node ${istanbul} cover ${mochaBin} -- --compilers .:${compiler} --require ${setup} ${mochaArgs}` :
