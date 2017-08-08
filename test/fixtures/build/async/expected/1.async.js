39e18c10156a285e6edbc6a6746ccee5c0d4d2f1
karzanOnline <3305001411@qq.com>
2017-08-08 15:36:04 +0800

    update async testcase

---
+++ test/fixtures/build/async/expected/1.async.js
@@ -0,0 +1,11 @@
+webpackJsonp([1],{
+
+/***/ 4:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\nexports.default = A;\nfunction A() {\n    return 'a';\n}\nmodule.exports = exports['default'];\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9zcmMvcm91dGVzL2EuanM/ODEzMiJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBBKCkge1xuICAgIHJldHVybiAnYSdcbn1cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gc3JjL3JvdXRlcy9hLmpzIl0sIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQUE7QUFDQTtBQUNBO0EiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///4\n");
+
+/***/ })
+
+});
---
+++ test/fixtures/build/async/src/index.js
@@ -0,0 +1,2 @@
+require('./routes/a');
+require('./routes/b');
