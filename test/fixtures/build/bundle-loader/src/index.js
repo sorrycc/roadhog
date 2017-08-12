ad0c03899279f50274118bbd9ebf376f00029f53
chencheng (云谦) <sorrycc@gmail.com>
2017-08-12 14:19:45 +0800

    add src directory to registerBabel (#376)

--- test/fixtures/build/bundle-loader/src/index.js
+++ test/fixtures/build/bundle-loader/src/index.js
@@ -1,19 +1,3 @@
-
-// import(/* webpackChunkName: 'a' */'./a').then(() => {
-//   console.log('a done');
-// });
-
-// import(/* webpackChunkName: 'b' */'./b').then(() => {
-//   console.log('b done');
-// });
-
-// Promise.all([
-//   import(/* webpackChunkName: 'a' */'./a'),
-//   import(/* webpackChunkName: 'b' */'./b')
-// ]).then(([a, b]) => {
-//   console.log(`all done: ${a}, ${b}`);
-// });
-
 require('bundle-loader?lazy&name=a!./a')((ret) => {
   console.log(`${ret} done`);
 });
