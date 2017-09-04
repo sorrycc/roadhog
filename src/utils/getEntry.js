60232b31bd2c03f8982bad5dcbb999fab58db58f
Eason <tobewhatwewant@gmail.com>
2017-09-04 15:30:53 +0800

    :bug: utils.getEntry with hotModuleReload. (#415)

--- src/utils/getEntry.js
+++ src/utils/getEntry.js
@@ -50,13 +50,16 @@ export default function (config, appDirectory, isBuild) {
       return entry;
     }
 
-    return Object.keys(entry).reduce((memo, key) => ({
+    return Object.keys(entry).reduce((memo, key) => (!Array.isArray(entry[key]) ? ({
       ...memo,
       [key]: [
         require.resolve('react-dev-utils/webpackHotDevClient'),
         entry[key],
       ],
-    }), {});
+    }) : ({
+      ...memo,
+      [key]: entry[key],
+    })), {});
   }
   const files = entry ? getFiles(entry, appDirectory) : [DEFAULT_ENTRY];
   return getEntries(files, isBuild);
