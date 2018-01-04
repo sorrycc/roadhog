28b7187b5249ba2360aed3d43cca39f4a539fad2
chencheng <sorrycc@gmail.com>
2018-01-04 12:42:57 +0800

    fix syntax problem in node@8, again, ref #538

---
+++ src/roadhog.js
@@ -0,0 +1,53 @@
+import chalk from 'chalk';
+import { fork } from 'child_process';
+
+require('graceful-process')();
+
+const script = process.argv[2];
+const args = process.argv.slice(3);
+
+const nodeVersion = process.versions.node;
+const versions = nodeVersion.split('.');
+const major = versions[0];
+const minor = versions[1];
+
+if (major * 10 + minor * 1 < 65) {
+  console.log(`Node version must >= 6.5, but got ${major}.${minor}`);
+  process.exit(1);
+}
+
+const scriptAlias = {
+  server: 'dev',
+};
+const aliasedScript = scriptAlias[script] || script;
+switch (aliasedScript) {
+  case '-v':
+  case '--version':
+    const pkg = require('../package.json');
+    console.log(pkg.version);
+    if (!(pkg._from && pkg._resolved)) {
+      console.log(chalk.cyan('@local'));
+    }
+    break;
+  case 'build':
+  case 'dev':
+  case 'test':
+    require('atool-monitor').emit();
+    const proc = fork(
+      require.resolve(`../lib/scripts/${aliasedScript}`),
+      args,
+      {
+        stdio: 'inherit',
+      },
+    );
+    proc.once('exit', code => {
+      process.exit(code);
+    });
+    process.once('exit', () => {
+      proc.kill();
+    });
+    break;
+  default:
+    console.log(`Unknown script ${chalk.cyan(script)}.`);
+    break;
+}
