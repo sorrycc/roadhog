3c76d47a794bf165ddbf5358620e3532f0732594
chencheng <sorrycc@gmail.com>
2018-01-08 15:44:38 +0800

    update example

--- examples/dead-simple/src/index.js
+++ examples/dead-simple/src/index.js
@@ -1,1 +1,20 @@
-console.log('app');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="full-page-content">
+        <div className="landing-blurb">
+          <div className="logo">Logo here</div>
+          <div className="animation">Something here</div>
+          <h1 ref="landingLabel" className="label">
+            I'm looking for things to do in
+          </h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
