d8d80ad275f27632e70b1ae56a0f715fff6229b2
157158 <xuzhan.xz@antfin.com>
2018-02-08 10:59:00 +0800

    cheap-module-source-map is replaced by cheap-module-source-map

--- src/utils/getDevtool.js
+++ src/utils/getDevtool.js
@@ -4,8 +4,8 @@ export default function getDevtool(devtool) {
   // https://github.com/webpack-contrib/uglifyjs-webpack-plugin
   // cheap-source-map options don't work with this plugin
   if (devtool === 'cheap-source-map') {
-    console.log(chalk.red('cheap-source-map options don\'t work with uglifyjs-webpack-plugin, cheap-module-source-map is recommended.'));
-    process.exit(1);
+    console.log(chalk.yellow('⚠️ cheap-source-map options don\'t work with uglifyjs-webpack-plugin, now cheap-module-source-map is set.'));
+    return 'cheap-module-source-map';
   }
 
   if (devtool.indexOf('eval') >= 0) {
