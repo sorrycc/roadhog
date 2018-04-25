3e312e194cd89030d85ffc81cd9c0a285c8cf709
spark <jdz321@163.com>
2018-04-25 22:25:10 +0800

    fix build error

--- scripts/build.js
+++ scripts/build.js
@@ -17,7 +17,7 @@ const nodeBabelConfig = {
         },
       },
     ],
-    require.resolve('@babel/preset-stage-0'),
+    [require.resolve('@babel/preset-stage-0'), { decoratorsLegacy: true }],
   ],
 };
 const browserBabelConfig = {
@@ -29,7 +29,7 @@ const browserBabelConfig = {
       },
     ],
     require.resolve('@babel/preset-react'),
-    require.resolve('@babel/preset-stage-0'),
+    [require.resolve('@babel/preset-stage-0'), { decoratorsLegacy: true }],
   ],
 };
 
