291f51d627188e65eb344108f950057f78f9a0d2
ikobe <ikobe621@gmail.com>
2018-11-29 21:34:48 +0800

    feat: support bigbrother report

--- src/build.js
+++ src/build.js
@@ -1,9 +1,11 @@
-import { resolve } from 'path';
+import { resolve, join } from 'path';
 import build from 'af-webpack/build';
 import getConfig from 'af-webpack/getUserConfig';
 import getWebpackConfig from './getWebpackConfig';
 import getPaths from './getPaths';
 import registerBabel from './registerBabel';
+import BuildStatistics from 'build-statistics-webpack-plugin';
+import BigBrother from 'bigbrother-webpack-plugin';
 
 const debug = require('debug')('roadhog:build');
 
@@ -12,6 +14,11 @@ export default function(opts = {}) {
 
   const babel = resolve(__dirname, './babel.js');
   const paths = getPaths(cwd);
+  const stagesPath = join(
+    __dirname,
+    '../.run/build-statistics/compilation.json',
+  );
+  const roadhogPkg = require(join(__dirname, '../package.json'));
 
   return new Promise(resolve => {
     // register babel for config files
@@ -32,6 +39,20 @@ export default function(opts = {}) {
       paths,
       entry,
     });
+
+    webpackConfig.plugins.push(
+      new BuildStatistics({
+        path: stagesPath,
+      }),
+      new BigBrother({
+        cwd,
+        tool: {
+          name: 'roadhog',
+          version: roadhogPkg.version,
+          stagesPath,
+        },
+      }),
+    );
 
     build({
       webpackConfig,
